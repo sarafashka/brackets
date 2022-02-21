63e4c0638752bf5f5b1df9d398b77fb040ea572b
Natalya Stepanova <natalyastepanova@MacBook-Air-Natalya.local>
2022-02-21 23:28:44 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    const currentChar = str[i];
+    for (let bracket of bracketsConfig) {
+      let topEl = stack[stack.length-1];
+      if (currentChar == bracket[0] && currentChar == bracket[1]) { 
+        if (topEl == currentChar) {
+          stack.pop()
+        } else {
+          stack.push(currentChar)
+        }
+      } else if (currentChar == bracket[0]) {
+        stack.push(currentChar)
+      } else if (currentChar == bracket[1]) {
+        if (topEl == bracket[0]){
+          stack.pop()
+        } else return false  
+      } 
+    }
+  }
+  return stack.length === 0
 }
